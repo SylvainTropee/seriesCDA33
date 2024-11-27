67164ac2cf652e3f78193369ddf1fd9b3c1e47d5
stropee <stropee@campus-eni.fr>
2024-11-27 12:29:56 +0100

    module 5 - récupération series + tempalte detail et liste

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,27 +13,67 @@ use Symfony\Component\Routing\Attribute\Route;
 class SerieController extends AbstractController
 {
     #[Route('', name: 'list', methods: ['GET'])]
-    public function list(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
+        $series = $serieRepository->findAll();
+
+//        $series = $serieRepository->findBy([], ["popularity" => "DESC"], 30, 20);
+
         //TODO renvoyer la liste de nos séries
-        return $this->render('serie/list.html.twig');
+        return $this->render('serie/list.html.twig', [
+            'series' => $series
+        ]);
     }
 
     #[Route('/{id}', name: 'detail', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
-    public function detail(int $id): Response
+    public function detail(int $id, SerieRepository $serieRepository): Response
     {
 
-        //TODO renvoyer une série spcécifique
-        return $this->render('serie/detail.html.twig');
+        $serie = $serieRepository->find($id);
+
+        //TODO renvoyer une série spécifique
+        return $this->render('serie/detail.html.twig', [
+            'serie' => $serie
+        ]);
     }
 
     #[Route('/add', name: 'add', methods: ['GET', 'POST'])]
-    public function add(): Response
+    public function add(EntityManagerInterface $entityManager): Response
     {
+
+        $serie = new Serie();
+        $serie
+            ->setBackdrop("backdrop.png")
+            ->setDateCreated(new \DateTime())
+            ->setName("The gentlemen")
+            ->setGenres("Gangsters")
+            ->setVote(8)
+            ->setFirstAirDate(new \DateTime('-1 year'))
+            ->setOverview("Un truc de gansters")
+            ->setPopularity(450)
+            ->setPoster('poster.png')
+            ->setStatus("returning")
+            ->setTmdbId(1234);
+
+        dump($serie);
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+
+        dump($serie);
+
+        $serie->setName("The gentlewomen");
+        $entityManager->flush();
+
+        dump($serie);
+
+        $entityManager->remove($serie);
+        $entityManager->flush();
+
+
         //TODO renvoyer un formulaire d'ajout de série
         return $this->render('serie/add.html.twig');
     }
-
 
 
 }
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Serie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 50; $i++) {
+
+            $serie = new Serie();
+            $serie
+                ->setName($faker->word())
+                ->setStatus($faker->randomElement(["returning", "ended", "canceled"]))
+                ->setPoster("poster.png")
+                ->setTmdbId($faker->randomDigit())
+                ->setPopularity($faker->numberBetween(1, 1000))
+                ->setVote($faker->numberBetween(0, 10))
+                ->setFirstAirDate($faker->dateTimeBetween("- 2 year", "- 6 month"));
+            $serie->setDateCreated(new \DateTime())
+                ->setBackdrop("backdrop.png")
+                ->setGenres($faker->randomElement(["Western", "SF", "Fantasy", "Drama"]))
+                ->setLastAirDate($faker->dateTimeBetween($serie->getFirstAirDate()));
+
+            $manager->persist($serie);
+        }
+
+        $manager->flush();
+    }
+}
